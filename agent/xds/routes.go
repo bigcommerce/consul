be90cf907d3ecfc0d72ca32c9f72478c6f28f505
Chris Boulton <chris@chrisboulton.com>
2021-01-13 21:43:45 -0800

    connect(bigcommerce): force all egress routes to 0s request timeouts (ingress controls timeouts)

--- agent/xds/routes.go
+++ agent/xds/routes.go
@@ -844,6 +844,7 @@ func makeRouteActionFromName(clusterName string) *envoy_route_v3.Route_Route {
 			ClusterSpecifier: &envoy_route_v3.RouteAction_Cluster{
 				Cluster: clusterName,
 			},
+			Timeout: durationpb.New(0),
 		},
 	}
 }
@@ -893,6 +894,7 @@ func (s *ResourceGenerator) makeRouteActionForSplitter(
 					TotalWeight: makeUint32Value(10000), // scaled up 100%
 				},
 			},
+			Timeout: durationpb.New(0),
 		},
 	}, nil
 }
