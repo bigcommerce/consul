32d84c20fe24b0ce700097c65b0b24344f6afa74
Chris Boulton <chris@chrisboulton.com>
2021-01-13 21:43:45 -0800

    connect(bigcommerce): force all egress routes to 0s request timeouts (ingress controls timeouts)

--- agent/xds/xds_protocol_helpers_test.go
+++ agent/xds/xds_protocol_helpers_test.go
@@ -794,6 +794,7 @@ func makeTestRoute(t *testing.T, fixtureName string) *envoy_route_v3.RouteConfig
 									ClusterSpecifier: &envoy_route_v3.RouteAction_Cluster{
 										Cluster: "db.default.dc1.internal.11111111-2222-3333-4444-555555555555.consul",
 									},
+									Timeout: durationpb.New(time.Duration(0)),
 								},
 							},
 						},
@@ -820,6 +821,7 @@ func makeTestRoute(t *testing.T, fixtureName string) *envoy_route_v3.RouteConfig
 									ClusterSpecifier: &envoy_route_v3.RouteAction_Cluster{
 										Cluster: "db.default.dc1.internal.11111111-2222-3333-4444-555555555555.consul",
 									},
+									Timeout: durationpb.New(time.Duration(0)),
 								},
 							},
 						},
